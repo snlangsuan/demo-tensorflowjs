71a2adf48d0e6ad5fb51c59125d312465424eaf9
decimo.me <snlangsuan@gmail.com>
2022-03-10 23:10:00 +0700

    change face detection model

--- pages/emotion.vue
+++ pages/emotion.vue
@@ -45,6 +45,7 @@
 
 <script>
 import numeral from 'numeral'
+import * as faceapi from '@vladmandic/face-api'
 export default {
   name: 'HomePage',
   data() {
@@ -99,9 +100,13 @@ export default {
       if (!this.unsupported) {
         try {
           this.loading = true
-          this.faceModel = await this.$blazeface.load()
+          // this.faceModel = await this.$blazeface.load()
+          // this.faceModel = new faceapi.SsdMobilenetv1()
+          // await this.faceModel.load('/demo-tensorflowjs/models/face-mobilenet-v1')
+          await faceapi.loadSsdMobilenetv1Model('/demo-tensorflowjs/models/face-mobilenet-v1')
+          this.faceModel = true
           this.emotionModel = await this.$tf.loadLayersModel(
-            'https://snlangsuan.github.io/demo-tensorflowjs/models/emotion/model.json'
+            '/demo-tensorflowjs/models/emotion/model.json'
           )
           this.startCam()
         } catch (error) {
@@ -166,43 +171,37 @@ export default {
         this.countDelay += 100
         // this.videoContext.fillRect(20, 20, 200, 200)
         if (this.faceModel && this.emotionModel) {
-          const frame = this.videoContext.getImageData(
-            this.mX,
-            this.mY,
-            this.mWidth,
-            this.mHeight
-          )
+          // const frame = this.videoContext.getImageData(
+          //   this.mX,
+          //   this.mY,
+          //   this.mWidth,
+          //   this.mHeight
+          // )
           // this.faces.push(this.toDataURL(frame))
-          const predictions = await this.faceModel.estimateFaces(frame)
-          // console.log(predictions)
-          // if (this.countDelay >= this.delayDetection) this.faces = []
-          for (const item of predictions) {
-            const probability = item.probability[0]
-            if (probability <= 0.6) continue
-            // console.log(item)
-            const start = item.topLeft
-            const end = item.bottomRight
-            const size = [end[0] - start[0], end[1] - start[1]]
-            // this.videoContext.fillRect(start[0], start[1],size[0], size[1])
-            // this.videoContext.beginPath()
-            // this.videoContext.strokeStyle= 'red'
-            // this.videoContext.lineWidth = 4
-            // this.videoContext.rect(start[0], start[1],size[0], size[1])
-            // this.videoContext.stroke()
+          // const predictions = await this.faceModel.estimateFaces(frame)
+          const predictions = await faceapi.detectAllFaces(this.videoCanvas, new faceapi.SsdMobilenetv1Options())
+          const predictionsResize = faceapi.resizeResults(predictions, { width: this.mWidth, height: this.mHeight })
+          for (const item of predictionsResize) {
+            const score = item.score
+             if (score <= 0.6) continue
+            const start = item.box.topLeft
 
-            // if (this.countDelay >= this.delayDetection) {
-            const landmark = item.landmarks
-            // const nosex = landmark[2][0]
-            // const nosey = landmark[2][1]
-            // const right = landmark[4][0]
-            // const left = landmark[5][0]
-            // const length = (left - right) / 2 + 5
-            // const frame2 = this.videoContext.getImageData(nosex - length, nosey - length, 2 * length, 2 * length)
+            this.videoContext.beginPath()
+            this.videoContext.strokeStyle = '#fefefe'
+            this.videoContext.lineWidth = 4
+            this.videoContext.rect(
+              start.x,
+              start.y + this.mY,
+              item.box.width,
+              item.box.height
+            )
+            this.videoContext.stroke()
+
             const frame2 = this.videoContext.getImageData(
-              start[0],
-              start[1] + this.mY,
-              size[0],
-              size[1]
+              start.x,
+              start.y + this.mY,
+              item.box.width,
+              item.box.height
             )
             let imageTensor = this.$tf.browser
               .fromPixels(frame2, 3)
@@ -216,42 +215,118 @@ export default {
             const result = this.emotionModel.predict(imageTensor)
             const predictedValue = result.arraySync()
             const maxIdx = this.argMax(predictedValue[0])
+
             if (maxIdx >= 0 && predictedValue[0][maxIdx] > 0.6) {
               const label = this.emotions[maxIdx]
               const emotionProb = predictedValue[0][maxIdx]
               const color = maxIdx === 0 ? 'red' : 'green'
+              // console.log(label, predictedValue[0][maxIdx])
 
               this.videoContext.beginPath()
               this.videoContext.strokeStyle = color
               this.videoContext.lineWidth = 4
               this.videoContext.rect(
-                start[0],
-                start[1] + this.mY,
-                size[0],
-                size[1]
+                start.x,
+                start.y + this.mY,
+                item.box.width,
+                item.box.height
               )
               this.videoContext.stroke()
 
-              landmark.forEach((x) => {
-                this.videoContext.fillRect(x[0], x[1] + this.mY, 5, 5)
-              })
-
               this.videoContext.fillStyle = color
               this.videoContext.fillRect(
-                start[0],
-                start[1] + this.mY,
-                size[0],
+                start.x,
+                start.y + this.mY,
+                item.box.width,
                 30
               )
               this.videoContext.fillStyle = 'white'
               this.videoContext.fillText(
                 label + ' (' + numeral(emotionProb).format('0.00%') + ')',
-                start[0] + 10,
-                start[1] + this.mY + 20
+                start.x + 10,
+                start.y + this.mY + 20
               )
             }
-            // }
+
           }
+          // console.log(predictions)
+          // if (this.countDelay >= this.delayDetection) this.faces = []
+          // for (const item of predictions) {
+          //   const probability = item.probability[0]
+          //   if (probability <= 0.6) continue
+          //   // console.log(item)
+          //   const start = item.topLeft
+          //   const end = item.bottomRight
+          //   const size = [end[0] - start[0], end[1] - start[1]]
+          //   // this.videoContext.fillRect(start[0], start[1],size[0], size[1])
+          //   // this.videoContext.beginPath()
+          //   // this.videoContext.strokeStyle= 'red'
+          //   // this.videoContext.lineWidth = 4
+          //   // this.videoContext.rect(start[0], start[1],size[0], size[1])
+          //   // this.videoContext.stroke()
+
+          //   // if (this.countDelay >= this.delayDetection) {
+          //   const landmark = item.landmarks
+          //   // const nosex = landmark[2][0]
+          //   // const nosey = landmark[2][1]
+          //   // const right = landmark[4][0]
+          //   // const left = landmark[5][0]
+          //   // const length = (left - right) / 2 + 5
+          //   // const frame2 = this.videoContext.getImageData(nosex - length, nosey - length, 2 * length, 2 * length)
+          //   const frame2 = this.videoContext.getImageData(
+          //     start[0],
+          //     start[1] + this.mY,
+          //     size[0],
+          //     size[1]
+          //   )
+          //   let imageTensor = this.$tf.browser
+          //     .fromPixels(frame2, 3)
+          //     .resizeBilinear([96, 96])
+          //     .mean(2)
+          //     .toFloat()
+          //     .expandDims(-1)
+          //   imageTensor = this.$tf.image
+          //     .grayscaleToRGB(imageTensor)
+          //     .expandDims(0)
+          //   const result = this.emotionModel.predict(imageTensor)
+          //   const predictedValue = result.arraySync()
+          //   const maxIdx = this.argMax(predictedValue[0])
+          //   if (maxIdx >= 0 && predictedValue[0][maxIdx] > 0.6) {
+          //     const label = this.emotions[maxIdx]
+          //     const emotionProb = predictedValue[0][maxIdx]
+          //     const color = maxIdx === 0 ? 'red' : 'green'
+
+          //     this.videoContext.beginPath()
+          //     this.videoContext.strokeStyle = color
+          //     this.videoContext.lineWidth = 4
+          //     this.videoContext.rect(
+          //       start[0],
+          //       start[1] + this.mY,
+          //       size[0],
+          //       size[1]
+          //     )
+          //     this.videoContext.stroke()
+
+          //     landmark.forEach((x) => {
+          //       this.videoContext.fillRect(x[0], x[1] + this.mY, 5, 5)
+          //     })
+
+          //     this.videoContext.fillStyle = color
+          //     this.videoContext.fillRect(
+          //       start[0],
+          //       start[1] + this.mY,
+          //       size[0],
+          //       30
+          //     )
+          //     this.videoContext.fillStyle = 'white'
+          //     this.videoContext.fillText(
+          //       label + ' (' + numeral(emotionProb).format('0.00%') + ')',
+          //       start[0] + 10,
+          //       start[1] + this.mY + 20
+          //     )
+          //   }
+          //   // }
+          // }
           // if (this.countDelay >= this.delayDetection) this.countDelay = 0
         }
         // if (this.control) window.requestAnimationFrame(this.predictWebcam)
